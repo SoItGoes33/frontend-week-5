f261d974980a77b43877f2992433f62c99276d24
SoItGoes33 <smf341@nyu.edu>
2015-09-09 20:21:04 -0700

    exercise 5 complete

--- code-appetizer/script.js
+++ code-appetizer/script.js
@@ -1,6 +1,11 @@
-/*
-
-  Your Javascript here
-
-*/
-
+function changeImage () {
+    var image = document.getElementById('myImage');
+    if (image.src.match("ANd9GcTdrnbfQB4F2k2J9EGjIvaBZBoM0wOkVowgSMnmCSqNOGyS_BQSPo8NVRo")) {
+        image.src = "http://people.ucsc.edu/~ddiep1/puppy.jpg";
+    }
+        else if (image.src.match('puppy')) {
+        image.src="http://vignette4.wikia.nocookie.net/animalcrossing/images/e/e3/Lion-013-2048x2048.jpg/revision/latest?cb=20130406213028";
+    }       else {
+        image.src = "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTdrnbfQB4F2k2J9EGjIvaBZBoM0wOkVowgSMnmCSqNOGyS_BQSPo8NVRo";
+    }
+}
--- exercise-1/script.js
+++ exercise-1/script.js
@@ -1,5 +1,12 @@
-/*
-
-  Your Javascript here
-
-*/
+function addListItem() {
+    var listitem = document.getElementById("listitem").value;
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(listitem);
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    node.appendChild(checkbox);
+    node.appendChild(textnode);
+    document.getElementById("list").appendChild(node);
+    event.preventDefault();
+    document.getElementById("listitem").value = '';
+}
